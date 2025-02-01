4a098d0f57215ee614fefec3ac78d72fe3533cab
Y.Miura <miura.mc23@gmail.com>
2025-02-01 12:03:40 +0900

    スクリプトを編集したらアプリを再起動する

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import os
 import sys
 import time
+import signal
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import subprocess
 
 class ChangeHandler(FileSystemEventHandler):
+    def __init__(self, process):
+        self.process = process
+
     def on_modified(self, event):
         if event.src_path.endswith('.py'):
             print(f'変更が検出されました: {event.src_path}')
+            if self.process:
+                print("既存のアプリを終了します...")
+                self.process.terminate()  # 既存のプロセスを終了
+                self.process.wait()  # プロセスが終了するのを待つ
+            print("新しいアプリを起動します...")
             os.execv(sys.executable, ['python'] + sys.argv)
 
 if __name__ == "__main__":
     path = '.'  # 現在のディレクトリを監視
-    event_handler = ChangeHandler()
+    process = subprocess.Popen(['python', 'src/app.py'])  # アプリを起動
+    event_handler = ChangeHandler(process)
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
-
-    # アプリを起動
-    subprocess.Popen(['python', 'src/app.py'])
 
     try:
         while True:
